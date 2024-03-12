4577dc21dd0a77bd42544f4d6952e02f50bd1c0b
kramskoy-v <kramskoy.va@gmail.com>
2024-03-12 23:20:01 +0300

    Статическая часть главной десктоп версии. Активное меню и faq

--- src/js/_custom-func.js
+++ src/js/_custom-func.js
@@ -1,1 +1,26 @@
-console.log(42);
+// меню
+const menu = document.querySelector('.menu');
+const btnMenu = document.querySelector('.btn-menu')
+
+btnMenu.addEventListener('click', () => {
+	btnMenu.classList.toggle('_active');
+	menu.classList.toggle('_active');
+})
+
+// аккордеон
+const questions = document.querySelectorAll('.faq__item');
+
+if (questions && questions.length > 0) {
+	questions[0].classList.add('_is-open');
+
+	questions.forEach((question) => {
+		question.addEventListener('click', () => {
+			questions.forEach((item) => {
+				if (item.classList.contains('_is-open')) {
+					item.classList.remove('_is-open');
+				}
+			});
+			question.classList.add('_is-open');
+		});
+	});
+}
