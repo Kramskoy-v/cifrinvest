36afe9dd40e17aaf88ad931fc43f1dd52784d377
kramskoy-v <kramskoy.va@gmail.com>
2024-03-13 22:14:21 +0300

    Добавлена анимация волн в карточки, на них же событие клика с модалкой. Адаптив от 768 полностью, от 375 частичный. Замена аватаров.

--- src/js/_custom-func.js
+++ src/js/_custom-func.js
@@ -1,26 +1,65 @@
 // меню
-const menu = document.querySelector('.menu');
-const btnMenu = document.querySelector('.btn-menu')
+const menu = document.querySelector(".menu");
+const btnMenu = document.querySelector(".btn-menu");
 
-btnMenu.addEventListener('click', () => {
-	btnMenu.classList.toggle('_active');
-	menu.classList.toggle('_active');
-})
+if (btnMenu) {
+	btnMenu.addEventListener("click", () => {
+		btnMenu.classList.toggle("_active");
+		menu.classList.toggle("_active");
+	});
+}
 
 // аккордеон
-const questions = document.querySelectorAll('.faq__item');
+const questions = document.querySelectorAll(".faq__item");
 
 if (questions && questions.length > 0) {
-	questions[0].classList.add('_is-open');
+	questions[0].classList.add("_is-open");
 
 	questions.forEach((question) => {
-		question.addEventListener('click', () => {
+		question.addEventListener("click", () => {
 			questions.forEach((item) => {
-				if (item.classList.contains('_is-open')) {
-					item.classList.remove('_is-open');
+				if (item.classList.contains("_is-open")) {
+					item.classList.remove("_is-open");
 				}
 			});
-			question.classList.add('_is-open');
+			question.classList.add("_is-open");
 		});
 	});
+}
+
+// открытие модалки при клике на карточки волн
+const cardWaveBtns = document.querySelectorAll(".cards__item");
+const overlay = document.querySelector(".overlay");
+const modalWave = document.querySelector(".modal-wave");
+const closeModal = document.querySelector(".modal__close-btn");
+
+const toggleOverlay = () => {
+	if (overlay.classList.contains("_is-active")) {
+		overlay.classList.remove("_is-active");
+		modalWave.classList.remove("_is-active");
+		document.body.style.overflow = null;
+	} else {
+		overlay.classList.add("_is-active");
+		modalWave.classList.add("_is-active");
+		document.body.style.overflow = "hidden";
+	}
+};
+
+if (cardWaveBtns) {
+	cardWaveBtns.forEach((cardBtn) => {
+		cardBtn.addEventListener("click", () => {
+			toggleOverlay();
+		});
+	});
+}
+
+document.addEventListener("keydown", (event) => {
+	if (event.code === "Escape" && overlay.classList.contains("_is-active")) {
+		toggleOverlay();
+	}
+});
+
+if (overlay || closeModal) {
+	overlay.addEventListener("click", toggleOverlay);
+	closeModal.addEventListener("click", toggleOverlay);
 }
